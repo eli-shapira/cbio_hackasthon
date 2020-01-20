ba6098b65e8d4a85801fb2a760cad18d7dc8ecf4
ninaberko <50692204+ninaberko@users.noreply.github.com>
2020-01-20 17:38:36 +0200

    Update Protein.py

--- Protein.py
+++ Protein.py
@@ -1,8 +1,18 @@
 class Protein:
 
-    def __init__(self,name,aa_seq,group_seq,keywords,structure):
+    def __init__(self, name, aa_seq, group_seq, keywords, structure):
         self.name = name
-        self.aa_seq = aa_seq
-        self.group_seq = group_seq
+        self.aa_seq = aa_seq[:-1]
+        self.len = len(self.aa_seq)
+        self.group_seq = group_seq[:-1]
+        self.group_seq = '0' + self.group_seq + '1'
         self.keywords = keywords
-        self.structure = structure
+        self.structure = self.replace_states(structure)[:-1]
+        self.to_drop = False
+        other_count = self.structure.count('O')
+        if other_count / self.len > 0.85:
+            self.to_drop = False
+        self.structure = 's' + self.structure + 'e'
+
+    def replace_states(self, s):
+        return s.replace('H', 'A').replace('S', 'B').replace('o', 'O')
