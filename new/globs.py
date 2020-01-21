ce4fc1dab8210c26cbbc794d22ae4a7bebcf8990
giladam <giladam@cs.huji.ac.il>
2020-01-21 08:53:28 +0200

    new folder

---
+++ new/globs.py
@@ -0,0 +1,48 @@
+import copy
+import numpy as np
+try:
+    from scipy.misc import logsumexp
+except:
+    from scipy.special import logsumexp
+from numpy import logaddexp
+
+BOLD = '\033[01m'
+RED = "\033[91m"
+CYAN = "\033[96m"
+YELLOW = "\033[93m"
+PURPLE = "\033[35m"
+END = "\033[00m"
+
+STATES = ['s', 'A', 'B', 'T', 'O', 'e']
+STATE_TO_INDEX = {'s':0, 'A':1, 'B':2, 'T':3, 'O':4, 'e':5}
+NUM_STATES = len(STATES)
+
+# start, negative, positive, polar, hydrophobic, special, end
+EMISSIONS_GROUP = ['0', '1', '2', '3', '4', '5', '6']
+EMISSIONS_RAW = ['0', 'G', 'M', 'L', 'N', 'A', 'S', 'F', 'Y', 'T', 'I', 'W', 'Q', 'C', 'P', 'D', 'V', 'E', 'H', 'K', 'R', '1']
+EMISSIONS = EMISSIONS_GROUP
+NUM_EMISSIONS = len(EMISSIONS)
+
+def print_emissions(dict):
+    print(BOLD+RED+"\t", end='')
+    for em in EMISSIONS:
+        print(em+"\t", end='')
+    print("\n"+END, end="")
+    for st in STATES:
+        print(BOLD+RED+st+"\t"+END, end="")
+        for em in EMISSIONS:
+            print(CYAN+str(dict[st][em])+"\t"+END, end='')
+        print("\n", end="")
+    print("\n", end="")
+
+def print_transitions(dict):
+    print(BOLD+RED+"\t", end='')
+    for i in STATES:
+        print(i+"\t", end='')
+    print("\n"+END, end="")
+    for i in STATES:
+        print(BOLD+RED+i+"\t", end="")
+        for j in STATES:
+            print(CYAN+str(dict[i][j])+"\t"+END, end='')
+        print("\n", end="")
+    print("\n", end="")
