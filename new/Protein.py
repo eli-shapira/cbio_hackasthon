57945e0b7d857ebcf5040f85a49e50fa73e80b0e
giladam <giladam@cs.huji.ac.il>
2020-01-21 10:32:54 +0200

    keyword analysis

--- new/Protein.py
+++ new/Protein.py
@@ -7,8 +7,14 @@ class Protein:
         self.len = len(self.aa_seq)
         self.group_seq = group_seq[:-1]
         self.group_seq = '0' + self.group_seq + '6'
+
+        OMIT = {"Reference proteome", "3D-structure", "Direct protein sequencing"}
         self.keywords = keywords.split(';')
         self.keywords[-1] = self.keywords[-1].split('.\n')[0]
+        for i in range(len(self.keywords)):
+            self.keywords[i] = self.keywords[i].strip()
+        self.keywords = [self.keywords[i] for i in range(len(self.keywords)) if self.keywords[i] not in OMIT]
+        
         self.structure = self.replace_states(structure)[:-1]
         self.to_drop = False
         other_count = self.structure.count('O')
@@ -18,3 +24,23 @@ class Protein:
 
     def replace_states(self, s):
         return s.replace('H', 'A').replace('S', 'B').replace('o', 'O')
+        
+    def evaluate_prediction(self, pred):
+        self.prediction = pred
+        matches = 0
+        total = 0
+        for strc_true, strc_our in zip(self.structure, self.prediction):
+            for i in range(len(strc_true)):
+                if len(strc_true) != len(strc_our):
+                    print(strc_our)
+                    print(strc_true)
+                    print(len(strc_true))
+                    print(len(strc_our))
+                    print("oh no")
+                    exit(1)
+                if strc_our[i] == strc_true[i]:
+                    matches += 1
+                total += 1
+        self.score = matches/total
+        print("evaluated",self.name[:-1],":",self.score)
+        return matches/total
--- new/main.py
+++ new/main.py
@@ -3,39 +3,66 @@ from parser import *
 from trainer import *
 from algos import *
 
+def filter_by_keyword(proteins, kw):
+    return [p for p in proteins if kw in p.keywords]
+    
+def print_sorted_dict(dct):    
+    for kw in sorted(dct, key=dct.get)[::-1]:
+        print(RED,dct[kw],END,kw)
+
+def keyword_histogram(kw_lists):
+    dct = {}
+    for lst in kw_lists:
+        for i in range(len(lst)):
+            if lst[i] in dct.keys():
+                dct[lst[i]] += 1
+            else:
+                dct[lst[i]] = 1
+    print_sorted_dict(dct)
+
+    return dct
+
+def score_histogram(proteins, all_keywords):
+    counter = {}
+    for p in proteins:
+        for kw in p.keywords:
+            if kw in counter.keys():
+                counter[kw] += p.score
+            else:
+                counter[kw] = p.score
+    for kw in counter:
+        counter[kw] = counter[kw] / all_keywords[kw]
+    for kw in sorted(counter, key=counter.get)[::-1]:
+        print(CYAN,all_keywords[kw],RED,counter[kw],END,kw)
+
+    return counter
+
 
 def main():
 
     PATH = 'data/prot_data_human'
     proteins = parse_file(PATH)
+    print("len proteins:", len(proteins))
 
-    p_train = proteins[:10000]
-    p_test = proteins[501:800]
+    p_train = proteins[:3000]
+    p_test = proteins[100:200]
+
 
     transitions = init_transitions(p_train)
-    emissions_bi = init_emissions_bi(p_train)
-    #emissions = init_emissions(p_train)
+    #emissions_bi = init_emissions_bi(p_train)
+    emissions = init_emissions(p_train)
     
-    true = [p.structure for p in p_test]
-    seqs = [p.group_seq for p in p_test]
     print("testing")
-    pred_bi = batch_posterior(seqs, transitions, emissions_bi, True)
-    #pred = batch_posterior(seqs, transitions, emissions)
+    kw_lists = [p.keywords for p in p_test]
+    all_keywords = keyword_histogram(kw_lists)
+    
+    #pred_bi = batch_posterior(seqs, transitions, emissions_bi, True)
     #pred_viterbi = batch_viterbi(seqs, transitions, emissions)
-    pred = pred_bi
+    for p in p_test:
+        pred = calculate_posterior(p.group_seq, transitions, emissions)
+        p.evaluate_prediction(pred)
 
-    for i in range(len(seqs)):
-        try:
-            print(pred[i][:60])
-            print(true[i][:60])
-            print("======================================")
-        except IndexError:
-            continue
-
-    err = evaluate(true, pred)
-    print(err)
-
-
+    score_histogram(p_test, all_keywords)
 
 
 
